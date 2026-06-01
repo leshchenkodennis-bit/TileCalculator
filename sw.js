c1c8e0f484f510c90afa90e60880edd9d85f5c2c
leshchenkodennis-bit <leshchenko.dennis@gmail.com>
2026-06-01 20:03:51 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'tilecalc-v54';
+const CACHE = 'tilecalc-v55';
 const ASSETS = ['./', 'index.html', 'icon-192.png', 'icon-512.png', 'manifest.json'];
 
 self.addEventListener('install', e => {
